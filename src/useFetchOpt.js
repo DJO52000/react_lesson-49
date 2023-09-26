4130acdb99b579be498945cd4361fe173899e83b
Dorde Drazic <djo52000@gmail.com>
2023-09-26 16:58:21 +0200

    Bonus Done

---
+++ src/useFetchOpt.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+
+export function useFetch(url, options = {}) {
+    const [data, setData] = useState()
+    const [isError, setIsError] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        setData(undefined)
+        setIsError(false)
+        setIsLoading(true)
+
+        fetch(url, options)
+            .then((res) => res.json())
+            .then(setData)
+            .catch(() => setIsError(true))
+            .finally(() => setIsLoading(false))
+    }, [url])
+
+    return { data, isError, isLoading }
+}
